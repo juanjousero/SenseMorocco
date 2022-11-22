71707dba5815d34cebc1323072843726cdff0ea5
juanjousero <101984696+juanjousero@users.noreply.github.com>
2022-11-22 10:44:30 +0100

    Improved menu: hovering over list shows images. Improved several distances between titles

--- resources/JS/menu.js
+++ resources/JS/menu.js
@@ -1,5 +1,4 @@
 // Obtenemos el botón del menú del DOM
-
 const main = document.getElementById('main');
 const menuButton = document.getElementById('hamburger-header');
 const menuLine01 = document.querySelector('.line01');
@@ -8,9 +7,13 @@ const menuLine03 = document.querySelector('.line03');
 const header = document.getElementById('header');
 const footer = document.getElementById('footer');
 const menuDiv = document.getElementById('menu-container');
+const menuImagesArray = Array.from(document.getElementsByClassName('menu-image'));
+// const dreamCitiesItem = document.querySelector('.list-dream-cities');
+// const magicRegionsItem = document.querySelector('.list-magic-regions');
+// const fabulousFoodItem = document.querySelector('.list-fabulous-food');
+const listItemsArray = document.querySelectorAll('.list-items');
 
 // Definimos las funciones para que quede más modular el archivo
-
 const removeClass = (variable, className) => {
     variable.classList.remove(className);
 }
@@ -60,5 +63,37 @@ const menuAppearence = () => {
 }
 
 // Añadimos un escuchador de click al menu que activará la función
-
 menuButton.addEventListener('click', menuAppearence);
+
+// Configuramos que se haga hover para mostrar las imágenes
+const imagesDisappearence = (event) => {
+    menuImagesArray.forEach((image) => {
+        image.classList.remove('fade-in-images');
+    })
+}
+
+const imagesAppearence = (event) => {
+    console.log(event.target);
+    if (event.target == listItemsArray[0]) {
+        menuImagesArray[0].style.backgroundImage = 'url("./resources/Images/menuImages/foto-menu01.png")'; // Imagen de la izquierda
+        menuImagesArray[1].style.backgroundImage = 'url("./resources/Images/menuImages/foto-menu03.png")'; // Imagen de la derecha
+        menuImagesArray[2].style.backgroundImage = 'url("./resources/Images/menuImages/foto-menu02.png")'; // Imagen del centro
+    } else if (event.target == listItemsArray[1]) {
+        menuImagesArray[0].style.backgroundImage = 'url("./resources/Images/menuImages/foto-menu02.png")';
+        menuImagesArray[1].style.backgroundImage = 'url("./resources/Images/menuImages/foto-menu01.png")';
+        menuImagesArray[2].style.backgroundImage = 'url("./resources/Images/menuImages/foto-menu03.png")';
+    } else if (event.target == listItemsArray[2]) {
+        menuImagesArray[0].style.backgroundImage = 'url("./resources/Images/menuImages/foto-menu03.png")';
+        menuImagesArray[1].style.backgroundImage = 'url("./resources/Images/menuImages/foto-menu02.png")';
+        menuImagesArray[2].style.backgroundImage = 'url("./resources/Images/menuImages/foto-menu01.png")';
+    }
+    menuImagesArray.forEach((image) => {
+        image.classList.add('fade-in-images');
+    })
+}
+
+// Añadimos un escuchador a cada item de la lista para que podamos descubrir las imágenes
+listItemsArray.forEach((item) => {
+    item.addEventListener('mouseenter', imagesAppearence);
+    item.addEventListener('mouseleave', imagesDisappearence);
+})
